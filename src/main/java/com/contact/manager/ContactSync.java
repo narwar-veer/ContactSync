38106483572de487aaee54b51c3266ebd480e7cd
narwar-veer <103619383+narwar-veer@users.noreply.github.com>
2024-03-23 17:32:45 +0530

    File name correction

--- src/main/java/com/contact/manager/ContactSync.java
+++ src/main/java/com/contact/manager/ContactSync.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SmartContactApplication {
+public class ContactSync {
 
 	public static void main(String[] args) {
 		
-		SpringApplication.run(SmartContactApplication.class, args);
+		SpringApplication.run(ContactSync.class, args);
 		
 	}
 
